c088f3c77ada9ef0cb32b41ff3f83a1761dfd0d8
tunglamlqddb <tunglamlqddb@gmail.com>
2022-04-13 12:09:04 +0700

    test_gen.py

---
+++ draw.py
@@ -0,0 +1,29 @@
+import matplotlib
+import matplotlib.ticker as ticker
+import matplotlib.pyplot as plt
+import numpy as np
+
+res_path = 'outputs/...'
+
+def get_data(path):
+    with open(path,'r') as f:
+        data = f.readlines()
+        data = np.array(data)
+    return data
+
+def line_plot(res):
+    x = np.arange(res[0])
+
+    plt.plot(x, res[0], label = "ReBN")
+    plt.plot(x, res[1], label = "No-ReBN")
+    plt.xlabel("Epoch")
+    plt.ylabel("Val Loss")
+    plt.title('Performance of Generator w and w/o ReBN')
+    plt.legend()
+    plt.show()
+
+paths = [res_path+'/rebn', res_path+'/no-rebn']
+res = []
+for path in paths:
+    res.append(get_data(path))
+line_plot(res)
